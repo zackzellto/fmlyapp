9bdf3e8a528ebf93d7e659d0a5b5732a33960683
Zack Zellto <zackzelltodev@gmail.com>
2023-02-05 00:40:14 -0700

    Added QR via svg, formatted iphon mockup png

--- client/src/Pages/Home.tsx
+++ client/src/Pages/Home.tsx
@@ -1,43 +1,46 @@
+import { Typography } from "@material-ui/core";
 import { Box, Container } from "@mui/material";
 import Grid from "@mui/material/Grid";
 import { PrimaryButton } from "../components/Buttons/PrimaryButton";
 import iPhoneMockup from "../components/Images/iPhoneMockup.png";
 import "../Styles/HomeScreenStyles.css";
+import QRCode from ".././assets/qrcode-download-app.svg";
 
 type Props = {};
 
 const Home = (props: Props) => {
   return (
     <>
-      <Grid container xs={12}>
-        <Box>
+      <Container maxWidth="lg">
+        <Grid>
           <h1 className="title">
             Get organized with <span className="fmly-title">fmly</span>!
           </h1>
           <br />
-        </Box>
-        <Box>
-          <p className="homepage-info-pitch">
+        </Grid>
+        <Grid>
+          <Typography className="homepage-info-pitch">
             Our app makes family management much more efficient. Communicate,
             manage tasks, and always have peace of mind with location sharing,
             all in one platform.
-          </p>
+          </Typography>
           <br />
-        </Box>
-        <Box>
-          <p className="homepage-download-info-text">
+        </Grid>
+        <Grid>
+          <Typography className="homepage-download-info-text">
             Download now and experience stress-free household coordination.
-          </p>
-        </Box>
+          </Typography>
+        </Grid>
         <PrimaryButton text={"Try a Demo."} />
-        <Container maxWidth="md">
+        <Container>
           <img
             className="iPhone-mockup"
             src={iPhoneMockup}
             alt="iPhone 14 Mockup"
           />
         </Container>
-      </Grid>
+        <img className="QR-code" src={QRCode} alt="QR Code" />
+      </Container>
     </>
   );
 };
