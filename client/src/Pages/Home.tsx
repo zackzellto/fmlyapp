7546dcc8f24dfc60d363c0d084887b13f8578e0a
Zack Zellto <zackzelltodev@gmail.com>
2023-02-05 15:17:33 -0700

    Got the conditional rendering working for mockup

--- client/src/Pages/Home.tsx
+++ client/src/Pages/Home.tsx
@@ -1,14 +1,37 @@
 import { Typography } from "@material-ui/core";
 import { Box, Container } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2";
-import { PrimaryButton } from "../components/Buttons/PrimaryButton";
 import iPhoneMockup from "../components/Images/iPhoneMockup.png";
 import "../Styles/HomeScreenStyles.css";
 import QRCode from ".././assets/qrcode-download-app.svg";
 import AppStoreButton from ".././assets/ios-download-button.svg";
 import PlayStoreButton from ".././assets/android-download-button.svg";
+import styled from "styled-components";
 
 type Props = {};
+
+const DemoButton = styled.button`
+  background: #21f3ce;
+  border-radius: 2.5px;
+  border: none;
+  color: #000000;
+  font-family: Poppins, sans-serif;
+  font-weight: 700;
+  font-size: 17px;
+  height: 56px;
+  width: 250px;
+  position: relative;
+  text-transform: none;
+  z-index: 1;
+  &:hover {
+    background: #10d8c0;
+    color: #000000;
+  }
+  &:active {
+    background: #10d8c0;
+    color: #000000;
+  }
+`;
 
 const Home = (props: Props) => {
   return (
@@ -27,14 +50,17 @@ const Home = (props: Props) => {
             all in one platform.
           </Typography>
           <br />
+          <DemoButton className="demo-button">Try our Demo</DemoButton>
         </Grid>
         <Grid>
+          <img className="iPhone-mockup" srcSet={iPhoneMockup} alt="iPhone" />
           <Typography className="homepage-download-info-text">
-            Download now and experience stress-free household coordination.
+            Download now and experience stress-free household coordination!
           </Typography>
         </Grid>
-        <PrimaryButton text={"Try a Demo."} />
         <img className="QR-code" srcSet={QRCode} alt="QR Code" />
+
+        <Typography className="QR-code-text"> (Scan to download) </Typography>
         <Grid>
           <a href="">
             <img
