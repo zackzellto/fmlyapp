a8e09567e3d61c493bc95a823924612d242cf80c
Zack Zellto <zackzelltodev@gmail.com>
2023-02-05 14:13:30 -0700

    Added some appstore and playstore buttons

--- client/src/Pages/Home.tsx
+++ client/src/Pages/Home.tsx
@@ -1,17 +1,19 @@
 import { Typography } from "@material-ui/core";
 import { Box, Container } from "@mui/material";
-import Grid from "@mui/material/Grid";
+import Grid from "@mui/material/Unstable_Grid2";
 import { PrimaryButton } from "../components/Buttons/PrimaryButton";
 import iPhoneMockup from "../components/Images/iPhoneMockup.png";
 import "../Styles/HomeScreenStyles.css";
 import QRCode from ".././assets/qrcode-download-app.svg";
+import AppStoreButton from ".././assets/ios-download-button.svg";
+import PlayStoreButton from ".././assets/android-download-button.svg";
 
 type Props = {};
 
 const Home = (props: Props) => {
   return (
     <>
-      <Container maxWidth="lg">
+      <Container className="home-container" maxWidth="xl">
         <Grid>
           <h1 className="title">
             Get organized with <span className="fmly-title">fmly</span>!
@@ -32,14 +34,23 @@ const Home = (props: Props) => {
           </Typography>
         </Grid>
         <PrimaryButton text={"Try a Demo."} />
-        <Container>
-          <img
-            className="iPhone-mockup"
-            src={iPhoneMockup}
-            alt="iPhone 14 Mockup"
-          />
-        </Container>
-        <img className="QR-code" src={QRCode} alt="QR Code" />
+        <img className="QR-code" srcSet={QRCode} alt="QR Code" />
+        <Grid>
+          <a href="">
+            <img
+              className="app-store-button"
+              srcSet={AppStoreButton}
+              alt="App Store Button"
+            />
+          </a>
+          <a href="">
+            <img
+              className="play-store-button"
+              srcSet={PlayStoreButton}
+              alt="Play Store Button"
+            />
+          </a>
+        </Grid>
       </Container>
     </>
   );
