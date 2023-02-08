100d673113ea549cd7864442faf4ca4a5d34538e
Zack Zellto <zackzelltodev@gmail.com>
2023-02-08 10:55:23 -0700

    Scroll effect needs to be fixed on mobile

--- client/src/components/AppBar/DevUpdateScrollEffect.tsx
+++ client/src/components/AppBar/DevUpdateScrollEffect.tsx
@@ -1,3 +1,4 @@
+import { Grid } from "@material-ui/core";
 import React from "react";
 import "../../Styles/DevUpdateScrollAnimation.css";
 
@@ -6,15 +7,17 @@ type Props = {};
 const DevUpdateScrollEffect = (props: Props) => {
   return (
     <>
-      <div className="dev-update-bar">
-        <text className="marquee">
-          <span className="dev-update-text">
-            <text className="dev-update">Development Update: </text>
-            fmly is being developed by one developer at the moment. I will
-            provide updates consistantly about the state of the application!
-          </span>
-        </text>
-      </div>
+      <Grid container>
+        <div className="dev-update-bar">
+          <text className="marquee">
+            <span className="dev-update-text">
+              <text className="dev-update">Development Update: </text>
+              fmly is being developed by one developer at the moment. I will
+              provide updates consistantly about the state of the application!
+            </span>
+          </text>
+        </div>
+      </Grid>
     </>
   );
 };
