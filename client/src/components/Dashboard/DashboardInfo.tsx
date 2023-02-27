429825792eb3a3e9991fc48b294552a36a7b4162
Zack Zellto <zackzelltodev@gmail.com>
2023-02-26 20:25:59 -0700

    Adding some SVGs and info about the Dashboard

--- client/src/components/Dashboard/DashboardInfo.tsx
+++ client/src/components/Dashboard/DashboardInfo.tsx
@@ -2,17 +2,16 @@ import React from "react";
 import Grid from "@mui/material/Unstable_Grid2";
 import DashboardMockup from "./DashboardMockup";
 import "../../Styles/DashboardStyles.css";
+import InfoArrow from "../../assets/info-arrow.svg";
 
 const DashboardInfo = () => {
   return (
     <div>
       <Grid>The Dashboard</Grid>
-
-      <div className="dashboard-info-bg">
-        <Grid>
-          <DashboardMockup />
-        </Grid>
-      </div>
+      <img className="info-arrow" srcSet={InfoArrow}></img>
+      <Grid>
+        <DashboardMockup />
+      </Grid>
     </div>
   );
 };
--- client/src/components/Dashboard/DashboardMockup.tsx
+++ client/src/components/Dashboard/DashboardMockup.tsx
@@ -1,14 +1,26 @@
 import React from "react";
+import styled from "styled-components";
 import DashboardMockupSVG from "../../assets/fmly-dashboard.svg";
+
+const PurpleBG = styled.div`
+  background-color: #d1b2ff;
+  margin: auto;
+  height: 1100px;
+  width: 1300px;
+  padding: 10px;
+  border-radius: 20px;
+`;
 
 const DashboardMockup = () => {
   return (
     <div>
-      <img
-        className="dashboard-mockup-svg"
-        srcSet={DashboardMockupSVG}
-        alt="dashboard-mockup"
-      ></img>
+      <PurpleBG>
+        <img
+          className="dashboard-mockup-svg"
+          srcSet={DashboardMockupSVG}
+          alt="dashboard-mockup"
+        ></img>
+      </PurpleBG>
     </div>
   );
 };
