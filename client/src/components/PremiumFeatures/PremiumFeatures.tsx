b56745b6f3256229fa945547572a0125fd38f2fa
Zack Zellto <zacharyzellto@gmail.com>
2023-03-12 00:30:29 -0700

    Added find my fmly info to premium features

--- client/src/components/PremiumFeatures/PremiumFeatures.tsx
+++ client/src/components/PremiumFeatures/PremiumFeatures.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Grid from "@mui/material/Unstable_Grid2";
 import "../../Styles/PremiumFeaturesStyles.css";
 import fmfMap from "../..//assets/find-my-fmly-map.svg";
+import fmfText from "../..//assets/find-my-fmly.svg";
 
 type Props = {}
 
@@ -11,7 +12,13 @@ const PremiumFeatures = (props: Props) => {
       <div className='premium-features-title'>Premium Features</div>
       <Grid>
       <div className='green-circle-pf' ><img className='find-my-fmly-map' srcSet={fmfMap} alt='fmfMap' ></img> </div>
-      </Grid></div>
+      </Grid>
+      <Grid>
+       <img className='fmf-text-svg' srcSet={fmfText} ></img>
+       <div className='fmf-text' >A premium feature that allows all of your fmly members 
+to be found via GPS tracking*, providing peace of mind
+and comfort...Or to pick something up on their way home... :)  </div>
+        </Grid></div>
   )
 }
 
